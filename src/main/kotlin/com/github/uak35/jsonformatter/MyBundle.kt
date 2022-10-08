d0ef7988236810e1ff8f67cc31412aa3afc63ea5
GitHub Action <action@github.com>
2022-10-08 06:40:07 +0000

    Template cleanup

--- src/main/kotlin/com/github/uak35/jsonformatter/MyBundle.kt
+++ src/main/kotlin/com/github/uak35/jsonformatter/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.uak35.jsonformatter
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/uak35/jsonformatter/services/MyProjectService.kt
+++ src/main/kotlin/com/github/uak35/jsonformatter/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.uak35.jsonformatter.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.uak35.jsonformatter.MyBundle
 
 class MyProjectService(project: Project) {
 
